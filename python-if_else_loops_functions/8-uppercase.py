99e9192a0002dab716d8c83150aec812dc4ff7ce
EmaD3c <>
2024-09-04 16:34:41 +0200

    change print format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,6 @@ def uppercase(str):
     for char in str:
         # Check if character is a lowercase letter
         if 'a' <= char <= 'z':
-            # Convert to uppercase by subtracting 32 from ASCII value
-            print(chr(ord(char) - 32), end='')
-        else:
-            # Print non-lowercase characters as they are
-            print(char, end='')
-            
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
