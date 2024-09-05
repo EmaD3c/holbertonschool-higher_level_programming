51e13a8d960fcab3ca0bd3c4e0099049175d7808
EmaD3c <>
2024-09-05 11:31:01 +0200

    delete comment

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
-# Runs the code below only if this file is executed directly, not when imported
 if __name__ == "__main__":
-
-    from add_0 import add  # Import the add function from add_0.py
-
-    # Assign the values to variables a and b
-    a = 1
-    b = 2
-
-    # Print the result using string formatting
-    print("{} + {} = {}".format(a, b, add(a, b)))
+  from add_0 import add
+  a = 1
+  b = 2
+  print("{} + {} = {}".format(a, b, add(a, b)))
