efc5942120e12e933008213db99a9a46a2f2fdcf
EmaD3c <>
2024-09-05 11:34:38 +0200

    add comment

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+# Runs the code below only if this file is executed directly, not when imported
 if __name__ == "__main__":
     from add_0 import add
+    # Assign the values to variables a and b
     a = 1
     b = 2
+    # Print the result using string formatting
     print("{} + {} = {}".format(a, b, add(a, b)))
