138851812a18a099b30cea4c0726cb979fe26de6
EmaD3c <>
2024-09-10 21:26:15 +0200

    add  function that returns a key with the biggest integer value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    # Vérifier si le dictionnaire est vide ou None
+    if not a_dictionary:
+        return None
+
+    # Utilise la fonction max pour trouver la clé avec la valeur la plus élevée
+    max_key = max(a_dictionary, key=a_dictionary.get)
+
+    return max_key
