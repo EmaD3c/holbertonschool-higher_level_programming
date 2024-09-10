5dbf55577ebf8159e30a6aa0b4bbcb5702eadb90
EmaD3c <>
2024-09-10 18:55:05 +0200

    add function that returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    # Créer un nouveau dictionnaire pour stocker les résultats
+    new_dict = {}
+    # Parcourir les clés et valeurs du dictionnaire
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
