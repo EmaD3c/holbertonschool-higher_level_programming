6e7d1520e5e2911ba3a95de7c4b7c4f3dc6ff605
EmaD3c <>
2024-09-10 22:22:03 +0200

    change return

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     # Apply the map with the correct lambda function using the provided number
-    my_new_list = list(map(lambda x: x * number, my_list))
-    return my_new_list
+    return list(map(lambda x: x*number, my_list))
