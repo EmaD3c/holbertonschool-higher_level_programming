f918edb3fb649b9f60daeeec17ec8d3900089b41
EmaD3c <>
2024-09-10 11:23:29 +0200

    delete comment

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Use list comprehension to create a new matrix
-    new_matrix = [[element ** 2 for element in line] for line in matrix]
-
-    return new_matrix
+  new_matrix = [[element ** 2 for element in line] for line in matrix]
+  return new_matrix
