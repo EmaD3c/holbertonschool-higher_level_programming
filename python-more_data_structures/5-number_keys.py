bff6bbb1c117f6dd0b90957fc8f7737f61153936
EmaD3c <>
2024-09-10 14:10:40 +0200

    add function that returns the number of keys in a dictionary

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-  count = 0
-    for key in test_dict:
-        count += 1
-        if isinstance(test_dict[key], dict):
-            count += number_keys(test_dict[key])
-    return count
+    return len(a_dictionary)
