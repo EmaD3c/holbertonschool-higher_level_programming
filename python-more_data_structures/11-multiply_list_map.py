af967096ef2d0f7a668ceeb636bbc6b17e4f1270
EmaD3c <>
2024-09-10 22:35:48 +0200

    change indentation

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    # Apply the map with the correct lambda function using the provided number
     return list(map(lambda x: x*number, my_list))
