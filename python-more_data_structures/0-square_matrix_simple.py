06a2aa34ce32c681b2dc3007558147627ec2fcff
EmaD3c <>
2024-09-10 11:32:57 +0200

    add pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,14 +2,14 @@
 def square_matrix_simple(matrix=[]):
     # Initialize an empty list to hold the squared values
     ret = []
-    
+
     # Loop through each row in the matrix
     for row in matrix:
         # Square each element in the row and create a new row
         squared_row = [x ** 2 for x in row]
-        
+
         # Append the squared row to the result list
         ret.append(squared_row)
-    
+
     # Return the new matrix with squared values
     return ret
