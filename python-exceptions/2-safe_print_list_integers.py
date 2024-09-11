ad5b7fd269c5f4b445ac69d8cf787a26cb656f44
EmaD3c <>
2024-09-11 15:01:17 +0200

    change except max len for error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-    except IndexError:
-        # Gérez le cas où x est plus grand que la longueur de la liste
+    except IndexError (TypeError, ValueError):
         pass
     print()
     return count
