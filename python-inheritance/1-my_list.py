1c7e4be29cbac894b75c2993abc2c54397fd342d
EmaD3c <>
2024-09-23 10:20:12 +0200

    Write a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+Write a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """Class that inherits from list"""
+    def print_sorted(self):
+         """Prints the list, but sorted in ascending order"""
+         print(sorted(self))
