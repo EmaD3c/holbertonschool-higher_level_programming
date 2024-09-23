edb08ac715df832b7207074101bb29ab9ec10b1e
EmaD3c <>
2024-09-23 11:14:45 +0200

    change == on is

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,4 +8,4 @@ Module 2-is_same_class
 
 def is_same_class(obj, a_class):
     """Returns True if obj is exactly an instance of a_class"""
-    return type(obj) == a_class
+    return type(obj) is a_class
