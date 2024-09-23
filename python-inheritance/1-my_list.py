b3856989efc3827602c1749ae5bbcc72768c8677
EmaD3c <>
2024-09-23 10:23:07 +0200

    add test + pycodestyle

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,7 @@ Write a class MyList that inherits from list
 
 class MyList(list):
     """Class that inherits from list"""
+
     def print_sorted(self):
-         """Prints the list, but sorted in ascending order"""
-         print(sorted(self))
+        """Prints the list, but sorted in ascending order"""
+        print(sorted(self))
