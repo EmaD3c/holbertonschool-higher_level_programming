7f70c0fd5d9dd085d7e3f178faa6a41976d40c6a
EmaD3c <>
2024-09-23 11:33:35 +0200

    change != for is not

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ or indirectly) from the specified class; otherwise False.
 
 def inherits_from(obj, a_class):
     """Returns True if obj inherits from a_class (but is not exactly a_class)"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
