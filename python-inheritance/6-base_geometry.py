c7243d5c203ccfd0605a9c47829d76c9c392bf8e
EmaD3c <>
2024-09-23 12:29:35 +0200

    write a class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Defines the class BaseGeometry with a method area.
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+
+    def area(self):
+        """Raises an Exception for unimplemented area method."""
+        raise Exception("area() is not implemented")
