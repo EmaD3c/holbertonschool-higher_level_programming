b6d03adc40c462a8320d0678d994436bfd1a5c62
EmaD3c <>
2024-09-23 14:35:48 +0200

    add import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,27 +5,22 @@ Defines the class Rectangle that inherits BaseGeometry.
 """
 
 
-class BaseGeometry:
-    """base geometry"""
-
-    def area(self):
-        """exception if area is not implemented"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """is an integer?"""
-        if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Rectangle"""
+
+    """
+    Class that represents a Rectangle, inherits from BaseGeometry
+    """
 
     def __init__(self, width, height):
-        """initialization method"""
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
+
+        """
+        init width and height
+        """
+
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
