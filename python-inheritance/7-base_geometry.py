2ecd9d441eb26e545a713463f804f39b67da32b9
EmaD3c <>
2024-09-23 12:38:18 +0200

    Write a class BaseGeometry (based on 6-base_geometry.py)

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 7-base_geometry
+Defines the class BaseGeometry with methods for area calculation and validation
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+
+    def area(self):
+        """Raises an Exception for unimplemented area method."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value as a positive integer."""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
