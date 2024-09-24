fd8b15f980eefdf06174399028e212cf095d18c7
EmaD3c <>
2024-09-24 16:06:09 +0200

    abstract class named Shape with two abstract methods: area and perimeter.

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Create an abstract class named Shape using the ABC package
+Create two subclasses of Shape
+"""
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """Abstract class Shape"""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """Circle class inherits from Shape"""
+
+    def __init__(self, radius):
+        """radius used to calculate both the area and the perimeter"""
+        self.radius = radius
+
+    def area(self):
+        return pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return (2 * self.radius) * pi
+
+
+class Rectangle(Shape):
+    """Rectangle class inherits from Shape"""
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+
+def shape_info(shape):
+    """Print the area and perimeter of the shape passed to the function"""
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
