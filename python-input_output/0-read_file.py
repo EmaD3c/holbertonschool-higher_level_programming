ff62caedd057fc9b01703ebd64af1f36fdcb9d2b
EmaD3c <>
2024-09-30 10:09:08 +0200

    add comment

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Reads a text file and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
+    """
+      Reads a text file and prints it to stdout.
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end="")
