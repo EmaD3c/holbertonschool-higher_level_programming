dd229b61df1a76d48753a5b0c51ceaee5be3a93f
EmaD3c <>
2024-10-24 21:25:32 +0200

    add script that creates the table id_not_null and add GRANT OPTION in create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
 -- Create the user 'user_0d_1' with password 'user_0d_1_pwd'
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Apply the changes (to make the grant effective)
+-- Apply the changes to make the grant effective
 FLUSH PRIVILEGES;
