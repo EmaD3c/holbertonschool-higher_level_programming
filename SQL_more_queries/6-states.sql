86d90bcea7f0a7986787f96f1bba518f2385adb0
EmaD3c <>
2024-10-25 10:25:12 +0200

    add Use database + into increment the id and declares id as the primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,11 @@
 -- Create the database 'hbtn_0d_usa' if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the database 'hbtn_0d_usa'
+USE hbtn_0d_usa;
+
 -- Create the table 'states.
 CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE DEFAULT 1 NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
