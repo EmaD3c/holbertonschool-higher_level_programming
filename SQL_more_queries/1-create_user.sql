e9a98fc1ab42cf98b4fced3f42763f677fd28fe9
EmaD3c <>
2024-10-25 10:20:39 +0200

    modify syntax error in create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Create the user 'user_0d_1' with password 'user_0d_1_pwd'
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges on all databases to the user
+-- Grant all privileges on all databases to 'user_0d_1'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes to make the grant effective
