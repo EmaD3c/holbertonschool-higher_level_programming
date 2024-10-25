3c4895d8837a1082fc860c4bff645b7bc1d838fa
EmaD3c <>
2024-10-25 16:38:43 +0200

    add script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,7 @@
-
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 USE hbtn_0d_tvshows;
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+-- récupérer les titres des émissions tv_shows.title en fonction des genres associés via l’identifiant de l’émission tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
