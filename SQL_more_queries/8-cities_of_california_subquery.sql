b215693e0c2dcf3f4a7befc58642c88ed5586219
EmaD3c <>
2024-10-25 11:18:44 +0200

    order by cities.id and not by only id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- Use the database 'hbtn_0d_usa'
 USE hbtn_0d_usa;
 
-SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
+SELECT cities.id, cities.name 
+FROM cities 
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
