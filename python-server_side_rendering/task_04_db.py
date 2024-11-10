8884742d524e48c000932343ab33dcdab539124d
EmaD3c <>
2024-11-10 17:05:56 +0100

    a

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -91,30 +91,22 @@ def read_sql_data(filename):
 # Route affiche les produits en fonction de la source et de l'id facultatif
 @app.route('/products', methods=['GET'])
 def display_products():
-    # récupère la source (json, csv, ou une source invalide)
     source = request.args.get('source', '')
-    # récupère l'id
     product_id = request.args.get('id', type=int)
     message = None
     products = []
 
-    # Si la source n'est pas json ou csv
     if source not in ['json', 'csv', 'sql']:
         message = "Wrong source"
 
     try:
         if source == 'json':
-            # ouvre le fichier json et assigne le contenu à products
             with open('products.json', 'r') as file:
                 products = json.load(file)
 
         elif source == 'csv':
-            # ouvre le fichier csv et convertie les infos en dict
-            # avant de l'écrire dans la variable reader
             with open('products.csv', 'r') as file:
                 reader = csv.DictReader(file)
-                # créer une liste contenant chaque ligne
-                # du CSV sous forme de dictionnaire
                 products = [row for row in reader]
 
         elif source == 'sql':
@@ -136,15 +128,10 @@ def display_products():
 
             print(f"Products fetched: {products}")
 
-        # Si id n'est pas NULL (donc si un id a été fournis)
         if product_id:
-            # remplace la liste products par une nouvelle liste
-            # contenant le product dans products qui a une clé "id"
-            # correspondant à product_id
             products = [
                 product for product in products if product['id'] == product_id]
             print(f"Filtered Products: {products}")
-            # Si la correspondance n'existe pas
             if not products:
                 message = "Product not found."
 
