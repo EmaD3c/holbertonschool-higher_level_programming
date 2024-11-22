50c4ebec810d22b4c352062a90b89adf2cbece12
Duupain <emadecot03@gmail.com>
2024-11-22 14:12:41 +0100

    change the selector

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,4 +1,4 @@
 function change_color() {
-  const color = document.getElementById("header").style.color = "#FF0000";
+  const color = document.querySelector("header").style.color = "#FF0000";
   return color
 }
