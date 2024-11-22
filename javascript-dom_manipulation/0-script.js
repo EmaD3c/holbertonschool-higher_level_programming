6d456cddf83f232ee6294ec655ca3b315cae945c
Duupain <emadecot03@gmail.com>
2024-11-22 14:33:41 +0100

    delete the function for optimisation

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,5 +1,1 @@
-function change_color() {
-  const color = document.querySelector("header").style.color = "#FF0000";
-  return color;
-};
-change_color()
+const color = document.querySelector("header").style.color = "#FF0000";
